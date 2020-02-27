d8261723c795e1328956de6380465aba673621a6
Sheldon Juncker <sheldonjuncker@gmail.com>
2020-02-27 15:23:56 -0500

    Adding prod/dev flags to web.

--- web/index.php
+++ web/index.php
@@ -1,8 +1,14 @@
 <?php
 
 // comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+
+require_once __DIR__ . '/../utilities/Settings.php';
+
+$env = \app\utilities\Settings::get('app', 'env') ?: 'dev';
+$debug = boolval(\app\utilities\Settings::get('app', 'debug'));
+
+defined('YII_DEBUG') or define('YII_DEBUG', $debug);
+defined('YII_ENV') or define('YII_ENV', $env);
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
